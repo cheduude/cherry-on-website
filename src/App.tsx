98008cc94d3e9d29fe510b5b879a04143336f698
cheduude <coookcok@gmail.com>
2026-01-15 17:04:18 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ const App: React.FC = () => {
   return (
     
     <NotificationProvider>
-      
       <ScrollToTop />
       <Suspense fallback={<div className="loading">Загрузка...</div>}>
         <Routes>
