1f77629c9b1ab2c00184deaba5d9f4d2b06e320d
cheduude <coookcok@gmail.com>
2026-01-19 23:38:29 +0300

    add themes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MainLayout from './components/Layout/MainLayout/MainLayout';
 import AuthLayout from './components/Layout/AuthLayout/AuthLayout';
 import ScrollToTop from './components/Pages/Home/ScrollToTop';
 import AdminAccessPage from './components/Pages/Admin';
+import './styles/themes.css';
 const Home = React.lazy(() => import('./components/Pages/Home/Home'));
 const Services = React.lazy(() => import('./components/Pages/Services/Services'));
 const Auth = React.lazy(() => import('./components/Pages/Auth/Auth'));
